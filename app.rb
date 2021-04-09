aaf86663b4f662c4ed07e6d03d350724a93a07dd
Benjamin Belaga <benjaminbelaga@Mac-mini-de-Benjamin.local>
2021-04-09 18:01:57 +0200

    fin release-en-tete

--- app.rb
+++ app.rb
@@ -3,29 +3,72 @@ require_relative 'lib/builder'
 require 'spreadsheet'
 require 'nokogiri'
 
+
 workbook = Spreadsheet.open 'data/label_worx.xls'
 worksheets = workbook.worksheets
 ws = worksheets[0]
 $catalog_current = ws.rows[1][1]
+$price = 1.29
 
-def checkCurrentCatalog?(catalog)
-    if catalog == nil
-        return false
+def getGenres(package)
+    genres = package[0][13].to_s
+    package.each do |genre|
+        if genres != genre[13]
+            genres = genres + "|#{genre[13]}"
+            break
+        end
     end
-    if $catalog_current == catalog
-        return true
-    else
-        $catalog_current = catalog
-        return false
-    end
+   return genres
 end
 
+def getYears(date1)
+    tab = Array.new
+    tab = date1.split('/')
+    return tab[2]
+end
 
+def getPrice(package)
+    price = 0
+    package.each {|song| price = price + $price}
+    return price.round(2)
+end
+
+def buildDataTrack(package)
+    playlist = ""
+    i = 1
+    package.each do |son|
+        playlist = son[8].nil? ? playlist + "<a href=\"http:\/\/player.yoyaku.io/mp3/#{son[1]}_#{i}.mp3\">#{son[6]} - #{son[7]}</a>\n" : playlist + "<a href=\"http:\/\/player.yoyaku.io/mp3/#{son[1]}_#{i}.mp3\">#{son[6]} - #{son[7]} (#{son[9]})</a>\n"
+        i = i + 1
+    end
+    return playlist
+end
 
 def sendPackage(package,xml)
+i = 1
     xml.release{
+        xml.title package[0][3]
+        xml.sku_ep package[0][1]
+        xml.artists package[0][2]
+        xml.label package[0][0]
+        xml.genres getGenres(package)
+        xml.years getYears(package[0][4])
+        xml.type "release"
+        xml.price getPrice(package)
+        xml.description package[0][5]
+        xml.owners "label_worx"
+        xml.product_visibility "visible"
+        xml.featured_image "https://www.aze.digital/wp-content/uploads/#{getYears(package[0][4])}/#{package[0][1]}"
+        xml.download_file_name "#{package[0][2]} - #{package[0][3]} #{package[0][1]}.wav"
+        xml.playlist_data buildDataTrack(package)
+        
         package.each do |row|
-            xml.title row[1]
+            xml.post{
+                xml.title row[4]
+              
+                xml.artists row[7]
+                xml.label row[0]
+            
+            }
         end
     }
 end
@@ -53,28 +96,3 @@ end
 
 
 build_xml(ws)
-=begin
-worksheets[0].rows.each do |row|
-    pp row[1]
-    i = i + 1
-end
-
-def getYears(date1)
-    tab = Array.new
-    if date1
-        tab = date1.split('/')
-        return tab[2]
-    end
-end
-
-def getPlaylist(ws)
-    playlist = ""
-    i = 1
-    ws.rows.drop(1).each do |row|
-        playlist = row[15].nil? ? playlist + "<a href=\"http:\/\/player.yoyaku.io/mp3/#{row[1]}_#{i}.mp3\">#{row[2]} - #{row[3]}</a>\n" : "no"
-        i = 1 + i
-    end
-    return playlist
-end
-build_xml(worksheets[0])
-=end
